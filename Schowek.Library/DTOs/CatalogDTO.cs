af60c9b08bc8e66526c0937a18dca4d49e0e8c6d
Marcin Chmurowski <sisimoski@outlook.com>
2023-02-21 22:54:18 +0100

    added CatalogDTO

---
+++ Schowek.Library/DTOs/CatalogDTO.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using Schowek.Library.Models;
+using Schowek.Library.Models.Enums;
+
+namespace Schowek.Library.DTOs
+{
+    public class CatalogDTO
+    {
+        [Required(ErrorMessage = "Nie wprowadzono nazwy. Wprowadź nazwę schowka.")]
+        [MinLength(1, ErrorMessage = "Za krótka nazwa.")]
+        public string? CatalogName { get; set; }
+        public string? Description { get; set; }
+        public string? Icon { get; set; }
+        public Colors? CatalogColor { get; set; }
+    }
+}
